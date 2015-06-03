25f363b88be4f407493876ffacc4b06de7689d00
talitalobo <talitabac@gmail.com>
2015-06-03 17:22:13 -0300

    runmusic com angular

---
+++ runmusic/js/app.js
@@ -0,0 +1,2 @@
+/* um módulo contem os diferentes componentes do aplicativo */
+var app = angular.module("runmusic", []);
---
+++ runmusic/js/mainController.js
@@ -0,0 +1,5 @@
+/* os controllers são responsáveis por gerenciar os dados do aplicativo */
+
+app.controller('MainController', ['$scope', function($scope) { 
+  $scope.title = 'RunMusic'; 
+}]);
